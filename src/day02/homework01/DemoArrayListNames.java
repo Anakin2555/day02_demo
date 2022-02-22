d96702556647986e5af6381cf344aaa06156dfbf
wai <2540994187@users.noreply.github.com>
2022-02-22 16:35:22 +0800

    张鲁刚创建分支

--- src/day02/homework01/DemoArrayListNames.java
+++ src/day02/homework01/DemoArrayListNames.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+/*
+*
+* 张鲁刚创建分支
+* 修改并合并至主分支
+*
+* */
+
+
 public class DemoArrayListNames {
     public static void main(String[] args) {
         List<String> one = new ArrayList<>();
